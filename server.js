7d9ab8b43f389a70e4e6134e268df0b49c7eab17
Matt <matthewbertweb@gmail.com>
2020-08-25 15:41:02 -0400

    added all dependencies, config file, and set up server file.

---
+++ server.js
@@ -0,0 +1,16 @@
+// Set up express server
+const express = require('express');
+// bring in database
+//const connectDB = require('./config/db');
+
+// Initialize app variable with express
+const app = express();
+
+// res.send will send data to the browser
+app.get('/', (req, res) => res.send('API Running'));
+
+// Set up PORT
+const PORT = process.env.PORT || 5000;
+
+// Listen on a port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
